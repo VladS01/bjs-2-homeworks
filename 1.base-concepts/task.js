74288e723134fcb11b7b0c346bced7790a9e7300
Vladislav Spesivcev <spesivtsevv01@yandex.ru>
2025-06-23 20:59:50 +0300

    Первое задание готово. Второе задание, кейс номер 2 не проходит

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,43 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = Math.pow(b, 2) - 4*a*c
+  if (D < 0) {
+    arr.push();
+  } else if (D == 0) {
+    let x = -b/(2*a)
+    arr.push(x);
+  } else {
+    let root1 = (-b + Math.sqrt(D))/(2*a);
+    let root2 = (-b - Math.sqrt(D))/(2*a);
+    arr.push(root1, root2);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+    // 1. Преобразуем процентную ставку
+    const monthlyPercent = (percent / 100) / 12;
+    
+    // 2. Рассчитываем тело кредита
+    const creditBody = amount - contribution;
+    
+    // 3. Проверяем, если тело кредита равно 0, возвращаем 0
+    if (creditBody <= 0) {
+        return 0;
+    }
+    
+    // 4. Рассчитываем ежемесячный платеж по формуле
+    const payment = creditBody * (
+        monthlyPercent + 
+        (monthlyPercent / ((Math.pow(1 + monthlyPercent, countMonths) - 1)))
+    );
+    
+    // 5. Рассчитываем общую сумму выплат
+    const totalPayment = payment * countMonths + contribution;
+    
+    // 6. Округляем результат до двух знаков после запятой
+    return parseFloat(totalPayment.toFixed(2));
+
 }
